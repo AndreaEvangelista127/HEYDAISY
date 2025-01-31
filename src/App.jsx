d3bfe3e174e49063d39bd513534b3257fb0b83f4
Andrea Evangelista <andro.evangelista127@gmail.com>
2025-01-31 18:53:30 +0100

    terzo tentativo 31/01

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './index.css';  
 
-function App() {
-  const [count, setCount] = useState(0)
-
+/** Dentro la funzione App, scrivi il tuo codice che assomiglia a HTML (ma in realtà è JSX). React lo trasformerà automaticamente in JavaScript per essere eseguito. **/
+function App() { //componente react che rappresenta la parte visibile del sito web
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Benvenuto su HEYDAISY! ciao</h1>
+      <p>Questo è il tuo sito per matrimoni!</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/* main.jsx è il file che prende App.jsx e lo inserisce dentro index.html, nel div con id root.*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> {/* Qui montiamo il componente App */}
   </StrictMode>,
 )
